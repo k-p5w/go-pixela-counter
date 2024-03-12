0303adc77f9724ba1c9cb4e63e3ba52d7b7956d0
k-p5w <53633693+k-p5w@users.noreply.github.com>
2024-03-13 00:14:06 +0900

    とりま動くようになったよ～～

--- README.md
+++ README.md
@@ -18,3 +18,16 @@ https://go-pixela-counter.vercel.app/api/ppppCounter
 https://go-pixela-counter.vercel.app/api/ppppCounter?incrementValue=-3&type=20240308
 
 https://go-pixela-counter.vercel.app/ppppCounter?incrementValue=-3&type=20240308
+
+### グラフURL
+
+https://pixe.la/v1/users/pppp-fan/graphs/popo-tsk-counter.svg
+
+
+https://pixe.la/v1/users/pppp-fan/graphs/popo-tsk-counter
+
+map[date:20240301 quantity:1]
+pppp-fan
+pppp20240312
+https://pixe.la/v1/users/%v/graphs/%v
+20240301
---
+++ public/fetch.js
@@ -0,0 +1,44 @@
+
+
+
+const fetchForm = document.querySelector('.fetchForm');
+const btn = document.querySelector('.btn');
+
+const userName = "pppp-fan"; // ユーザ名
+const graphId = "popo-tsk-counter"; // グラフID
+const pwd = "pppp20240312";
+
+
+
+const POST_URL = `https://pixe.la/v1/users/${userName}/graphs/${graphId}`; // POST-URL
+const postFetch = () => {
+    let formData = new FormData(fetchForm);
+    // 増減値を取得
+    let incrementValue = formData.get('incrementValue');
+
+    // 入力日を取得
+    let selectedDate = formData.get('selectedDate');
+    const dataBody = {
+        date: selectedDate,
+        quantity: incrementValue
+    };
+    fetch(POST_URL, {
+        method: 'POST',
+        headers: {
+            'X-USER-TOKEN': pwd,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataBody)
+    }).then((response) => {
+        if (!response.ok) {
+            console.log('error!');
+        }
+        console.log('ok!');
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+    }).catch((error) => {
+        console.log(error);
+    });
+};
+btn.addEventListener('click', postFetch, false);
